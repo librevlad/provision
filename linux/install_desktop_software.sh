179f5de3ff1a6295d8285a09fd56ca922f39de16
librevlad <librevlad@gmail.com>
2021-01-30 18:40:45 +0200

    w

--- linux/install_desktop_software.sh
+++ linux/install_desktop_software.sh
@@ -7,7 +7,7 @@ sudo apt -y install shutter chromium-browser i3lock terminator rofi vlc totem \
 kdenlive audacity telegram-desktop filezilla qbittorrent pavucontrol aircrack-ng reaver \
 guvcview remmina x11vnc wine64 playonlinux arc-theme lxappearance arandr ecryptfs-utils \
 samba xclip xsel gparted kodi breeze breeze-gtk-theme breeze-icon-theme byobu speedtest-cli \
-slack kodi-addons-dev shutter fonts-firacode pcmanfm feh scrot compton opera python-pip libreoffice blueman adb qemu-kvm davfs2 volti
+kodi-addons-dev shutter fonts-firacode pcmanfm feh scrot compton aircrack-ng reaver libreoffice blueman adb qemu-kvm davfs2 volti gmtp
 
 sudo snap install phpstorm --classic
 sudo snap install skype --classic
--- linux/install_musthave_software.sh
+++ linux/install_musthave_software.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install CLI software
-sudo apt -y install fish fzf vim php-cli php-gd php-curl php-pdo-mysql php-bcmath php-zip php-pdo-mysql php-dom php-mbstring composer python-pip \
+sudo apt -y install fish fzf vim php-cli php-gd php-curl php-pdo-mysql php-bcmath php-zip php-pdo-mysql php-dom php-mbstring composer python3-pip \
 nodejs npm git tmux htop docker.io docker-compose youtube-dl wget curl ssh findutils \
 tinc dirb tldr net-tools mlocate mysql-client nmap
 
