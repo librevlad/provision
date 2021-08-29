ad3dec986cdfa9d2af42a5606770ed41de3a3909
Dmytro Novik <40357628+LightFleet@users.noreply.github.com>
2021-08-29 21:44:12 +0300

    Update install_desktop_software.sh

--- linux/install_desktop_software.sh
+++ linux/install_desktop_software.sh
@@ -6,11 +6,11 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo add-apt-repository ppa:linuxuprising/shutter
 sudo apt-get update
 
-sudo apt -y install shutter i3lock terminator rofi vlc totem \
+sudo apt -y install i3lock terminator rofi vlc totem \
 kdenlive audacity telegram-desktop filezilla qbittorrent pavucontrol aircrack-ng reaver \
 guvcview remmina x11vnc wine64 playonlinux arc-theme lxappearance arandr ecryptfs-utils \
 samba xclip xsel gparted kodi breeze breeze-gtk-theme breeze-icon-theme byobu speedtest-cli \
-kodi-addons-dev shutter fonts-firacode pcmanfm feh scrot compton aircrack-ng reaver libreoffice blueman adb qemu-kvm davfs2 gmtp pasystray
+kodi-addons-dev fonts-firacode pcmanfm feh scrot compton aircrack-ng reaver libreoffice blueman adb qemu-kvm davfs2 gmtp pasystray
 
 sudo snap install phpstorm --classic
 sudo snap install skype --classic
